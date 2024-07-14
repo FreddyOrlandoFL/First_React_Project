43791d196ac79f1414bb2c2811cee00e2a913450
FreddyOrlandoFL <freddy.figueroa@avalith.net>
2024-07-14 19:19:53 -0400

    Eventos
    Eventos
    Funciones como valores Props

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,12 +1,16 @@
+import './TabButton.css'
 export default function TabButton({children,label}){
   /*si funcion se llama el props TabButton(props) => {props.children} pero puedo enviarlo directo*/ 
   /*si funcion se llama la propiedad directamente TabButton({children}) => {children} pero puedo enviarlo directo
     si en el html se usa asi se llama children 
      <TabButton>Componentes</TabButton>
-  */  
+  */ 
+  function handleClick(){
+    console.log("Ey! Estas pulsando sobre uno de los botones del menu!");
+  } 
   return(
     <li>
-        <button label={label}>{children}</button>
+        <button onClick={handleClick} label={label}>{children}</button>
     </li>
   );   
 }
