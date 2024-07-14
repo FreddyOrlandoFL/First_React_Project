d1b237e0961a4bf8bffea94b31a0ee83a6c5b0d1
FreddyOrlandoFL <freddy.figueroa@avalith.net>
2024-07-14 19:38:19 -0400

    funciones en props
    funciones en props

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,16 +1,13 @@
-import './TabButton.css'
-export default function TabButton({children,label}){
-  /*si funcion se llama el props TabButton(props) => {props.children} pero puedo enviarlo directo*/ 
-  /*si funcion se llama la propiedad directamente TabButton({children}) => {children} pero puedo enviarlo directo
+/*si funcion se llama el props TabButton(props) => {props.children} pero puedo enviarlo directo 
+  si funcion se llama la propiedad directamente TabButton({children}) => {children} pero puedo enviarlo directo
     si en el html se usa asi se llama children 
      <TabButton>Componentes</TabButton>
-  */ 
-  function handleClick(){
-    console.log("Ey! Estas pulsando sobre uno de los botones del menu!");
-  } 
-  return(
+*/ 
+import './TabButton.css';
+export default function TabButton({children, onClick}) {
+  return (
     <li>
-        <button onClick={handleClick} label={label}>{children}</button>
+      <button onClick={onClick}>{children}</button>
     </li>
-  );   
+  );
 }
